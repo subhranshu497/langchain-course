3fb05151cda4a67fc6b10d7520253e970ba3e54d
subh <subhranshu497@gmail.com>
2026-02-16 16:12:16 -0700

    inti commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
+
+
 def main():
     print ("Hello From langchain Course")
-
 
 if __name__ == "__main__":
     main()
